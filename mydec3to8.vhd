c45dc805f39f134b711ed63c3f4b1545fe699aef
AlarkJani <109466449+AlarkJani@users.noreply.github.com>
2023-02-03 07:13:47 +0530

    Update mydec3to8.vhd

--- mydec3to8.vhd
+++ mydec3to8.vhd
@@ -1,12 +1,4 @@
----------------------------------------------------------------------------
--- This VHDL file was developed by Daniel Llamocca (2013).  It may be
--- freely copied and/or distributed at no cost.  Any persons using this
--- file for any purpose do so at their own risk, and are responsible for
--- the results of such use.  Daniel Llamocca does not guarantee that
--- this file is complete, correct, or fit for any particular purpose.
--- NO WARRANTY OF ANY KIND IS EXPRESSED OR IMPLIED.  This notice must
--- accompany any copy of this file.
---------------------------------------------------------------------------
+
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
