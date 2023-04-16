1eec86f75f23c6e43496eb5729819199a9b54c53
AlarkJani <109466449+AlarkJani@users.noreply.github.com>
2023-04-16 16:06:27 -0400

    Add files via upload

---
+++ Ajani5978_sr.v
@@ -0,0 +1,13 @@
+module Ajani5978_sr (input [7:0] sw, output[3:0] led, input c1k);
+reg[6:3] R;
+always@(posedge c1k)
+begin 
+case ({sw[1],sw[0]})
+2'b00 : R <= R;
+2'b01 : R <= {R[5:3], sw[2]};
+2'b10 : R <= {sw[7],R[6:4]};
+2'b11 : R <= sw[6:3];
+endcase
+end
+assign led=R;
+endmodule
